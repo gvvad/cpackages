51c567915bfcd4da7d293e63258ca0d57c2c7347
gvvad <gvvvad92@gmail.com>
2019-07-09 18:50:43 +0300

    commit

--- manual/qaac/tools/chocolateyInstall.ps1
+++ manual/qaac/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
-    packageName = "qacc"
+    packageName = $env:ChocolateyPackageName
     url = "https://github.com/nu774/qaac/releases/download/v2.68/qaac_2.68.zip"
     checksum = "8067826564d182a239a2347b40d52369c4a378b7df7918bd156138bf904168d0"
     checksumType = "sha256"
