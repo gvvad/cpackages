45f86287c1b2c00b65dd12c17b55b6d81098d09e
Vadym Hvozdik <gvvvad92@gmail.com>
2020-11-05 00:08:34 +0200

    rapr 0.11.56 update

--- manual/rapr/tools/chocolateyInstall.ps1
+++ manual/rapr/tools/chocolateyInstall.ps1
@@ -4,8 +4,8 @@ Import-Module $toolsPath\helpers.psm1
 
 $packageArgs = @{
     packageName = $env:ChocolateyPackageName
-    url = "https://github.com/lostindark/DriverStoreExplorer/releases/download/v0.11.42/DriverStoreExplorer.v0.11.42.zip"
-    checksum = "da2c207a4a1fa61b69c40654160d6fed419ba5f4c7d69ff882ddbe80abc61993"
+    url = "https://github.com/lostindark/DriverStoreExplorer/releases/download/v0.11.56/DriverStoreExplorer.v0.11.56.zip"
+    checksum = "fc71c0ad3a27e2c6f8bad89548db14d73dbda6ad6c2c19bee0ecfe0d0e56fee7"
     checksumType = "sha256"
     unzipLocation  = $toolsPath
 }
