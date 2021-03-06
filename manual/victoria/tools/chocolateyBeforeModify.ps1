919c3934819bde3fd9781022c415c562aa4d3df4
gvvad <gvvvad@gmail.com>
2021-03-06 21:43:46 +0200

    logic fix TODO

--- manual/victoria/tools/chocolateyBeforeModify.ps1
+++ manual/victoria/tools/chocolateyBeforeModify.ps1
@@ -2,5 +2,5 @@ $ErrorActionPreference = 'Stop';
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 Import-Module $toolsPath\helpers.psm1
 
-Get-Process -Name "victoria" -ErrorAction Ignore | Stop-Process -ErrorAction Ignore
+Get-Process -Name "victoria" -ErrorAction Ignore | Stop-Process -ErrorAction Ignore # TODO: In this case, need to throw "abort install" exception!
 Remove-Item -Path $startMenuPath -Recurse -ErrorAction Ignore
