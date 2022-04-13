609fbbbde4b8e486909d772ef895266681c3c402
gvvad <gvvvad@gmail.com>
2022-04-13 19:45:55 +0300

    rapr update

--- manual/rapr/tools/chocolateyInstall.ps1
+++ manual/rapr/tools/chocolateyInstall.ps1
@@ -3,19 +3,19 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 Import-Module $toolsPath\helpers.psm1
 
 $packageArgs = @{
-    packageName = $env:ChocolateyPackageName
-    url = "https://github.com/lostindark/DriverStoreExplorer/releases/download/v0.11.72/DriverStoreExplorer.v0.11.72.zip"
-    checksum = "0193e6bbdf85d4ec118b7ccecb6837bd6ed38a8cd193de924d8028003ae712c5"
-    checksumType = "sha256"
-    unzipLocation  = $toolsPath
+    packageName   = $env:ChocolateyPackageName
+    url           = "https://github.com/lostindark/DriverStoreExplorer/releases/download/v0.11.79/DriverStoreExplorer.v0.11.79.zip"
+    checksum      = "6dfcfc7ed5d98664e457f886a8a70403f6472db8d51e1b1bebc89a2facfe7dcb"
+    checksumType  = "sha256"
+    unzipLocation = $toolsPath
 }
 
 Install-ChocolateyZipPackage @packageArgs
 
-foreach($item in $extFiles.GetEnumerator()) {
+foreach ($item in $extFiles.GetEnumerator()) {
     Install-ChocolateyShortcut `
-    -ShortcutFilePath "$(Join-Path $startMenuPath $item.Name).lnk" `
-    -TargetPath $(Join-Path $toolsPath $item.Value) `
-    -WorkingDirectory $toolsPath `
-    -RunAsAdmin
+        -ShortcutFilePath "$(Join-Path $startMenuPath $item.Name).lnk" `
+        -TargetPath $(Join-Path $toolsPath $item.Value) `
+        -WorkingDirectory $toolsPath `
+        -RunAsAdmin
 }
--- manual/victoria/tools/chocolateyInstall.ps1
+++ manual/victoria/tools/chocolateyInstall.ps1
@@ -4,18 +4,19 @@ Import-Module $toolsPath\helpers.psm1
 
 $packageArgs = @{
     packageName = $env:ChocolateyPackageName
-    url = "https://hdd.by/Victoria/Victoria536.zip"
-    checksum = "c1a661f859a584b594716ba23e9d2f8e12261c2af513d107a0358bc25a6b898e"
+    url = "https://hdd.by/Victoria/Victoria537.zip"
+    checksum = "a66b7869d59bbf90b5994332bac57bcbc0b377cee9f0c59c078795ea7b4c99d1"
     checksumType = "sha256"
     unzipLocation  = $toolsPath
 }
 
 Install-ChocolateyZipPackage @packageArgs
 
+# different folder structure in this version. Add subfolder in lnk path.
 foreach($item in $extFiles.GetEnumerator()) {
     Install-ChocolateyShortcut `
     -ShortcutFilePath "$(Join-Path $startMenuPath $item.Name).lnk" `
-    -TargetPath "$(Join-Path $toolsPath $item.Value)" `
+    -TargetPath "$(Join-Path "$(Join-Path $toolsPath "Victoria537")" $item.Value)" `
     -WorkingDirectory "$toolsPath" `
     -RunAsAdmin
 }
